c8d3742ae211dcf7d9f5cd45fc54aa04c9ea24ab
Hipjea <pierre.duverneix@gmail.com>
2022-11-15 11:29:08 +0100

    add utils.ts

--- src/components/edit.tsx
+++ src/components/edit.tsx
@@ -1,13 +1,14 @@
 import React, { FunctionComponent } from 'react'
 import { EditProps } from '../types/Edit'
+import { PARTNERS_GRID_PARTNERS } from '../utils'
 
 
 const Edit: FunctionComponent<EditProps> = ({ ...props }: EditProps) => {
 	console.log(props)
 
-// @ts-ignore
-const PARTNERSGRIDLIST = PARTNERSGRID.partner;
-console.log(PARTNERSGRIDLIST)
+	// @ts-ignore
+	console.log(PARTNERS_GRID_PARTNERS);
+
 	return (
 		<div className="partners-grid">Bonjour 2</div>
 	)
