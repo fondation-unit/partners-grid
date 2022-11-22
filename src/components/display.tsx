9c89be82f0d04cf2ac77218d39f145c9c1d1fcdf
CarolineMVB <caroline.beauchet@yahoo.fr>
2022-11-22 10:42:31 +0100

    add activateElement() for reseau

--- src/components/display.tsx
+++ src/components/display.tsx
@@ -1,11 +1,21 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import { PartnerType } from '../types/Partner'
 import { PARTNERS_GRID_PARTNERS } from '../utils'
 
 
+
 const Display = () => {
-    const partners = JSON.parse(PARTNERS_GRID_PARTNERS); 
-	
+    const partners = JSON.parse(PARTNERS_GRID_PARTNERS)
+	const [active, setActive] = useState<string>('')
+
+	const activateElement = (name: string) => {
+		if (active == name) {
+			setActive('')
+		} else {
+			setActive(name)
+		}
+	}
+
 	return (
 		<div id='partnersgrid-block-root' className="partners-grid">
 			<div className='container'>
@@ -14,17 +24,23 @@ const Display = () => {
 						{ partners.map((element: PartnerType) => {
 							return (
 								element.type && element.type == "reseau"
-									? <div>
+									? <div className="reseau">
 										<h6>{element.name}</h6>
-                                        <img src={element.logo} alt={element.name}/>
-										{ console.log(element) }
-										{ element.items && element.items.map((item: any) => {
-											return (
-												<a href={item.url}>
-													<img src={item.logo} alt={item.name} className="img" />
-												</a>
-											)
-										}) }
+                                        <img 
+											src={element.logo} 
+											alt={element.name} 
+											onClick={() => activateElement(element.name)} 
+											className={ active === element.name ? `reseau-img active` : `reseau-img` } 
+										/>
+										<div className="reseau-items">
+											{ element.items && element.items.map((item: any) => {
+												return (
+													<a href={item.url} className="item">
+														<img src={item.logo} alt={item.name} className="img" />
+													</a>
+												)
+											}) }
+										</div>
 									</div>
 									
 									: <a href={element.url}>
