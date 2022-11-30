64edb2ebedd984ad1a1c01c51a9819193a14d244
CarolineMVB <caroline.beauchet@yahoo.fr>
2022-11-30 09:14:29 +0100

    activeId, setActiveId in Display

--- src/components/Reseau.tsx
+++ src/components/Reseau.tsx
@@ -1,34 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import * as React from 'react'
 import Partner from './Partner'
-import Icon from './IconPlus'
+import IconPlus from './IconPlus'
 import { PartnerType } from '../types/Partner'
 
+interface Props {
+    element: PartnerType
+    setActiveId: (name: any) => void
+    activeId: number
+}
 
-const Reseau = (element: PartnerType) => {
-	const [active, setActive] = useState<any>({})
-
-    useEffect(() => {
-        activateElement(element)
-    }, [])
-
-	const activateElement = (element: PartnerType) => {
-		setActive(active.name == element.name ? element : {})
-	}
+const Reseau = ({element, setActiveId, activeId}: Props) => {
+    const elementId = parseInt(element.id)
 
 	return (
         <div className="reseau">
             <img 
                 src={element.logo} 
                 alt={element.name}
-                onClick={ () => setActive(element === active ? {} : element) }
-                className={ active === element.name ? `reseau-img active-reseau` : `reseau-img` } 
+                onClick={ () => setActiveId(elementId === activeId ? 0 : elementId) }
+                className={ activeId === elementId ? `reseau-img active-reseau` : `reseau-img` } 
             />
-            <Icon element={element} setActiveElement={setActive} active={active} />
-            <div className="reseau-items">
-                { element.items && element.items.map((item: any) => {
-                    return <Partner {...item} />
-                }) }
-            </div>
+            <IconPlus 
+                element={element} 
+                setActiveId={setActiveId} 
+                activeId={activeId} 
+            />
+            { activeId === elementId ?
+                <div className="reseau-items">
+                    { element.items && element.items.map((item: any) => {
+                        return <Partner {...item} />
+                    }) }
+                </div>
+            : null }
         </div>			
 	)
 }
--- src/components/display.tsx
+++ src/components/display.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import Reseau from './Reseau'
 import Partner from './Partner'
 import { PartnerType } from '../types/Partner'
@@ -7,14 +7,15 @@ import { PARTNERS_GRID_PARTNERS } from '../utils'
 
 
 const Display = () => {
+	const [activeId, setActiveId] = useState<number>(0)
     const partners = JSON.parse(PARTNERS_GRID_PARTNERS)
-
+ 
 	return (
 		<div className='partner-container'>
 			{ partners.map((element: PartnerType) => {
 				return (
 					element.type && element.type == "reseau"
-						? <Reseau {...element} />
+						? <Reseau element={element} activeId={activeId} setActiveId={setActiveId} />
 						: <Partner {...element} />
 				)
 			}) }
