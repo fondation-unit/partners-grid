d9b32dc39ecd2bb16f55dc5f80ddf60c6da1b37f
CarolineMVB <caroline.beauchet@yahoo.fr>
2022-11-30 14:54:54 +0100

    edit partner.tsx

--- src/components/Partner.tsx
+++ src/components/Partner.tsx
@@ -3,14 +3,16 @@ import { ReseauProps } from '../types/Reseau'
 
 
 const Partner = ({element, setActiveId, activeId}: ReseauProps) => {
+    const elementId = parseInt(element.id)
+
 	return (
-        <div onClick={ () => setActiveId(element.id) }
-            className={ activeId === parseInt(element.id) ? `partner-img active-info` : `partner-img` } >
+        <div onClick={ () => setActiveId(elementId === activeId ? 0 : elementId) }
+            className={ activeId === elementId ? `partner-img active-info` : `partner-img` } >
             <img src={element.logo} alt={element.name} className='card__image' />
             <div className='card__overlay'>
                 <div className='card__header'></div>
                 <h6 className='card__title'>{element.name}</h6>
-                <a href={element.url} className='card__url'>Lien Back</a>
+                <a href={element.url} className='card__url'>Lien</a>
             </div>
         </div>	
     )
