e2d387069c434595d61ea4bd7c256af3650ccc1a
Hipjea <pierre.duverneix@gmail.com>
2023-01-16 13:43:27 +0100

    add target="_blank"

--- src/components/Partner.tsx
+++ src/components/Partner.tsx
@@ -2,12 +2,14 @@ import * as React from 'react'
 import { ReseauProps } from '../types/Reseau'
 
 
-const Partner = ({element}: ReseauProps) => {
+const Partner = ({ element }: ReseauProps) => {
 
-	return (
+    return (
         <div className="partner-img">
-            <a href={element.url}><img src={element.logo} alt={element.name} className='card__image' /></a>
-        </div>	
+            <a href={element.url} target="_blank">
+                <img src={element.logo} alt={element.name} className='card__image' />
+            </a>
+        </div >
     )
 }
 
